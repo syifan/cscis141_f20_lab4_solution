0309d406c5d5d73f37facb03c38f06a8eb0cb85f
Yifan Sun <sunyifan112358@gmail.com>
2020-10-06 15:32:06 +0000

    Finish weekday

--- cal.py
+++ cal.py
@@ -1,11 +1,46 @@
 from datetime import date
 from copy import deepcopy
 
+
 def weekday(month, day, year):
     '''
     Given a date as month (string), day (int), and year (int), return the weekday.
     '''
-    pass
+
+    month_table = {
+        "Mar": 3,
+        "Apr": 4,
+        "May": 5,
+        "Jun": 6,
+        "Jul": 7,
+        "Aug": 8,
+        "Sep": 9,
+        "Oct": 10,
+        "Nov": 11,
+        "Dec": 12,
+        "Jan": 13,
+        "Feb": 14,
+    }
+
+    day_table = {
+        0: "Sat",
+        1: "Sun",
+        2: "Mon",
+        3: "Tue",
+        4: "Wed",
+        5: "Thu",
+        6: "Fri",
+    }
+
+    m = month_table[month]
+
+    if month == "Jan" or month == "Feb":
+        year -= 1
+
+    day_of_week = (day + (13 * (m + 1)) // 5 + year + year // 4 - year // 100 + year // 400) % 7
+
+    return day_table[day_of_week]
+
 
 def is_leap_year(year):
     '''
@@ -13,14 +48,16 @@ def is_leap_year(year):
     '''
     pass
 
+
 def day_of_the_year(month, day, year):
     '''
     Given a date as month (string), day (int), and year (int), return the day of the year.
     '''
     pass
 
-#-----------------------------------------------------------
+# -----------------------------------------------------------
 # Pytest test code: execute with "pytest change.py".
+
 
 def test_is_leap_year():
     '''
@@ -28,6 +65,7 @@ def test_is_leap_year():
     '''
     assert is_leap_year(2020)
     assert not is_leap_year(1999)
+
 
 def test_weekday():
     '''
@@ -50,6 +88,7 @@ def test_weekday():
                 assert weekday(month, day, year) == DAYS[date(year, m, day).weekday()]
             m += 1
 
+            
 def test_day_of_year():
     '''
     More inadequate testing.  Test against the datetime module.
@@ -71,8 +110,9 @@ def test_day_of_year():
                 assert day_of_year(month, day, year) == date(year, m, day).toordinal() - date(year, 1, 1).toordinal() + 1
             m += 1
 
-#---------------------------------------------------------------------------
+# ---------------------------------------------------------------------------
 # Purely python (not pytest) test code: execute with "python change.py".
+
 
 # The following if statement insures the code does not execute when imported into other test code.
 if (__name__ == '__main__'):
